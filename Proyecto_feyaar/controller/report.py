85fa6263985bb6cf34833136fbe442e5c1af447c
feyaar <fyarasca@gmail.com>
2025-02-17 09:57:32 -0500

    Add files via upload

---
+++ Proyecto_feyaar/controller/report.py
@@ -0,0 +1,31 @@
+from config.app import *
+import pandas as pd
+
+# crear un reporte diferente
+def GenerateReportVentas(app:App):
+    conn=app.bd.getConection()
+    query="""
+        SELECT 
+            p.pais,
+            v.product_id,
+            SUM(v.quantity) AS total_vendido
+        FROM 
+            VENTAS v
+        JOIN 
+            POSTALCODE p
+        ON 
+            v.postal_code = p.code
+        GROUP BY 
+            p.pais, v.product_id
+        ORDER BY 
+            total_vendido DESC;
+    """
+    df=pd.read_sql_query(query,conn)
+    fecha="08-02"
+    path=f"/workspaces/workspacepy0125v2/proyecto/files/data-{fecha}.csv"
+    df.to_csv(path)
+    sendMail(app,path)
+
+def sendMail(app:App,data):
+    # cambiar el asunto 
+    app.mail.send_email('from@example.com','Reporte','Reporte',data)
